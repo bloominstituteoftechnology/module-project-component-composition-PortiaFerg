c5f3548ffa22793029103f9de7270fbff05e4041
Portia Ferguson <portiaroyalty@gmail.com>
2024-05-26 14:57:55 -0600

    done2

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,15 +1,38 @@
 import React from 'react'
 import Figure from './Figure'
+import styled from 'styled-components'
+
+const StyledCard = styled.div`
+border: 1px solid black;
+border-radius: 12px;
+padding: 2rem;
+
+h2 {
+    color: ${pr => pr.$color};
+    margin: 0 0 1rem 0;
+    font-size: 2.5em;
+}
+
+p {
+    margin: 0 0 1.5rem 0;
+
+    &::first-line {
+    font-size: 1.5rem;
+}
+}
+
+
+`
 
 export default function Card({ title, text, imageURL, date }) {
     return (
-  <div className='card'> 
+  <StyledCard $color="orange" className='card'> 
           <h2>{title}</h2>
           <p>{text}</p>
         <Figure
           imageURL={imageURL}
           caption={date}
         />
-        </div>
+        </StyledCard>
     )
   }
